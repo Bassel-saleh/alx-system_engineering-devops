bc753662aaba25ef498cc1c61f2bbe4817c7ecf7
Bassel-saleh <basselh26@gmail.com>
2024-03-22 04:43:00 -0700

    debugging

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask v(2.1.0)
-package { 'flask':
+# Installs flask v2.1.0 with pip3
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
