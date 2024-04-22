269687e23dd8e9c11ffc6702e8885926d389d54f
Bassel-saleh <basselh26@gmai.com>
2024-04-22 20:13:28 +0200

    added task#1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Exports TODO list information for any employee using his ID"""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = requests.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    todo = requests.get(url + "todos", params={"userId": user_id}).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todo]
