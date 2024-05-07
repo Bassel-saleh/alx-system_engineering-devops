65c78f47f0b89c49f9e8b5f099f4486721c086f6
Bassel-saleh <basselh26@gmai.com>
2024-05-07 18:09:43 +0300

    added task#1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" prints hot posts on a given Reddit subreddit """
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Print the titles of the 10 hottest posts on a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    rspns = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if rspns.status_code == 404:
+        print("None")
+        return
+    result = rspns.json().get("data")
+    [print(c.get("data").get("title")) for c in result.get("children")]
