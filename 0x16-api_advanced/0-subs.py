ee51cfb70fda9dbe925c8be8b9321eb91db71442
Bassel-saleh <basselh26@gmai.com>
2024-05-07 17:59:37 +0300

    debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
+    """
+    queries the Reddit API and returns the
+    number of subscribers for a given subreddit
+    """
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozzila/5.0"}
+    rspns = requests.get(url, headers=headers, allow_redirects=False)
+    if rspns.status_code == 404:
         return 0
+    return rspns.json().get("data").get("subscribers")
