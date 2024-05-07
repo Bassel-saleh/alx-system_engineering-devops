f879640ae31d8918cc5927d56c8badbded2a0ed9
Bassel-saleh <basselh26@gmai.com>
2024-05-07 18:00:28 +0300

    debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,6 +14,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozzila/5.0"}
     rspns = requests.get(url, headers=headers, allow_redirects=False)
-    if rspns.status_code == 404:
-        return 0
-    return rspns.json().get("data").get("subscribers")
+    if rspns.status_code == 200:
+        return rspns.json().get("data").get("subscribers")
+    return 0
