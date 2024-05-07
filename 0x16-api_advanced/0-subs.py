535c6d94279efd34ac41ddfe0cfb338953bd0f79
Bassel-saleh <basselh26@gmai.com>
2024-05-07 17:21:26 +0300

    debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,10 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the
     number of subscribers for a given subreddit
     """
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "BISSO/1.0"
-    }
+    headers = {"User-Agent": "BISSO/1.0"}
     rspns = requests.get(url, headers=headers, allow_redirects=False)
     if rspns.status_code == 404:
         return 0
-    result = rspns.json().get("data")
-    return result.get("subscribers")
+    return rspns.json().get("data").get("subscribers")
